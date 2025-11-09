8ab994f7f915bc0b08336983c12a6bc349eb7b8d
Morten S Eriksen <morteser@ntnu.no>
2025-11-09 15:01:57 +0100

    task 1.3

--- øving4/script.js
+++ øving4/script.js
@@ -5,7 +5,14 @@ console.log('Task  1.2')
 console.log('Task  1.3')
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
+for (let i of numbers) {
+    console.log(i)
+    if (i % 3 === 0) {
+        console.log('eple')
+    } else if (i % 5 === 0) {
+        console.log('kake')
+    }
+}
 /* Task  1.4 */
 
 /* Task  1.5 */
